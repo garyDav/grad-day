c44b8ad2652a258dcdca2a8d5500a568739fc9bb
garyDav <gary.2810.dav@gmail.com>
2024-09-27 16:49:45 -0400

    invitación terminada

--- src/routes/v1/home/controller.ts
+++ src/routes/v1/home/controller.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
+import { join } from 'path'
+import fs from 'fs'
 
 export const listHome = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -14,6 +16,13 @@ export const submitInvitation = async (req: Request, res: Response, next: NextFu
 
     console.log(`Nombre: ${name}`)
     console.log(`Asistirá con pareja: ${withPartner ? 'Sí' : 'No'}`)
+    const confirmationLine = `Name: ${name}, With Partner: ${withPartner ? 'Sí' : 'No'}\n`
+
+    const filePath = join(__dirname, '../../../public/lista.txt')
+
+    fs.appendFile(filePath, confirmationLine, err => {
+      console.log('saved', err)
+    })
 
     res.send(`Gracias por confirmar, ${name}.`)
   } catch (error: any) {
