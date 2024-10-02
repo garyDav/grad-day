39cb32055ea3fb7f7bba7352bc92d8df61978f6e
garyDav <gary.2810.dav@gmail.com>
2024-10-02 18:29:21 -0400

    invitacion terminada

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -198,7 +198,7 @@
   // const d = new Date(new Date().getTime() + 200 * 120 * 120 * 2000)
   // const d = new Date(2024, 9, 4)
   // const d = new Date(Date.UTC(2024, 9, 4, 16, 45))
-  const d = new Date(Date.UTC(2024, 9, 4, 16, 45) + 6 * 60 * 60 * 1000)
+  const d = new Date(Date.UTC(2024, 9, 4, 16, 45) + 9 * 60 * 60 * 1000)
 
   // default example
   simplyCountdown('.simply-countdown-one', {
